fa536a1c3412f0d164889a10b02d8711663e80a0
Ritaban Chaudhuri <inrcr27@gmail.com>
2025-07-07 19:14:12 +0530

    Create the base enemy plant script

--- Assets/Script/EnemyPlants/EnemyPlantBase.cs
+++ Assets/Script/EnemyPlants/EnemyPlantBase.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class EnemyPlantBase : MonoBehaviour
 {
+    [Header("Movement Settings")]
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private Transform[] moveSpots;
+    [SerializeField] private float waitTimeAtSpot = 1f;
+
+    private int currentSpotIndex = 0;
+    private float waitTimer;
+
     // Start is called before the first frame update
-    void Start()
+    protected virtual void Start()
     {
-        
+        waitTimer = waitTimeAtSpot;
     }
 
     // Update is called once per frame
-    void Update()
+    protected virtual void Update()
     {
-        
+        Patrol();
+    }
+
+    protected void Patrol()
+    {
+        if (moveSpots.Length == 0) return;
+
+        Transform targetSpot = moveSpots[currentSpotIndex];
+        transform.position = Vector2.MoveTowards(transform.position, targetSpot.position, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, targetSpot.position) < 0.2f)
+        {
+            if (waitTimer <= 0f)
+            {
+                currentSpotIndex = (currentSpotIndex + 1) % moveSpots.Length;
+                waitTimer = waitTimeAtSpot;
+            }
+            else
+            {
+                waitTimer -= Time.deltaTime;
+            }
+        }
     }
 }
