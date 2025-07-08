57ef8c7bdd5e712784511a1ee1bd5a17c1627613
Ritaban Chaudhuri <inrcr27@gmail.com>
2025-07-08 12:28:37 +0530

    created the stat component

---
+++ Assets/Script/Player/StatComponent.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum StatType { Lucidity, Coins }
+
+public class StatComponent : MonoBehaviour
+{
+    [SerializeField] private int lucidity;
+    [SerializeField] private int maxLucidity;
+    [SerializeField] private int coins;
+    public void IncreaseValue(StatType stat, int amount)
+    {
+        switch (stat)
+        {
+            case StatType.Lucidity:
+                lucidity += amount;
+                break;
+            case StatType.Coins:
+                coins += amount;
+                break;
+        }
+        lucidity = Mathf.Clamp(lucidity, 0, 100);
+        coins = Mathf.Max(0, coins);
+    }
+}
