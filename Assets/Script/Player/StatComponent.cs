8b69b67a62a6b2b3dfcd2a637363feafaf03867c
Ritaban Chaudhuri <inrcr27@gmail.com>
2025-07-15 20:36:24 +0530

    dec lucidity at a defined rate

--- Assets/Script/Player/StatComponent.cs
+++ Assets/Script/Player/StatComponent.cs
@@ -13,6 +13,11 @@ public class StatComponent : MonoBehaviour
     [SerializeField] private Image lucidityBar;
     [SerializeField] private TMP_Text coinText;
 
+    [SerializeField] private int lucidityDrainAmount = 5; // X
+    [SerializeField] private float lucidityDrainInterval = 2f; // Y
+
+    private float lucidityDrainTimer = 0f;
+
     private void Start()
     {
         UpdateUI();
@@ -46,6 +51,18 @@ public class StatComponent : MonoBehaviour
 
     private void Update()
     {
-        
+        if (lucidity > 0)
+        {
+            lucidityDrainTimer += Time.deltaTime;
+
+            if (lucidityDrainTimer >= lucidityDrainInterval)
+            {
+                lucidity -= lucidityDrainAmount;
+                lucidity = Mathf.Clamp(lucidity, 0, maxLucidity);
+                UpdateUI();
+
+                lucidityDrainTimer = 0f;
+            }
+        }
     }
 }
