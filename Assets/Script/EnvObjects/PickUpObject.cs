7353473ced6bd03a2407a39028c951b02f5e092f
Ritaban Chaudhuri <inrcr27@gmail.com>
2025-07-15 20:51:37 +0530

    add prefabs

--- Assets/Script/EnvObjects/PickUpObject.cs
+++ Assets/Script/EnvObjects/PickUpObject.cs
@@ -2,15 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Coin : MonoBehaviour
+public class PickUpObject : MonoBehaviour
 {
     private string PLAYER_TAG = "Player";
+    [SerializeField] private StatType pickUpType;
+    [SerializeField] private int pickUpValue;
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag(PLAYER_TAG))
         {
-            collision.GetComponent<StatComponent>().IncreaseValue(StatType.Coins, 1);
+            collision.GetComponent<StatComponent>().IncreaseValue(pickUpType, pickUpValue);
             Destroy(gameObject);
         }
     }
