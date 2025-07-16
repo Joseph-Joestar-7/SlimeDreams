029aeaf404db80cb4710d2fe606a16979ecd7aa9
Ritaban Chaudhuri <inrcr27@gmail.com>
2025-07-16 21:54:13 +0530

    Update GameInput.cs

--- Assets/Script/Player/GameInput.cs
+++ Assets/Script/Player/GameInput.cs
@@ -1,16 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
     private IA_Player playerInputActions;
-
+    public event EventHandler OnInteractAction;
     private void Awake()
     {
         playerInputActions = new IA_Player();
         playerInputActions.Player.Enable();
+
+        playerInputActions.Player.Interact.performed += Interact_performed;
     }
+
+    private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        OnInteractAction?.Invoke(this, EventArgs.Empty);
+    }
+
     void Start()
     {
 
