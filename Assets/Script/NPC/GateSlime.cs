1a83c26fe94da0c92b163492fc9de91afe9edeaa
Ritaban Chaudhuri <inrcr27@gmail.com>
2025-07-16 21:46:33 +0530

    create script for gatesslime

---
+++ Assets/Script/NPC/GateSlime.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateSlime : MonoBehaviour,I_Interactable
+{
+    public void Interact()
+    {
+       
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
