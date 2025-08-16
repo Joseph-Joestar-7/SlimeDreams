ffe015e482799e950cd103e568e5a71272e91836
Ritaban Chaudhuri <inrcr27@gmail.com>
2025-08-16 11:03:53 +0530

    Update GameInput.cs

--- Assets/Script/Player/GameInput.cs
+++ Assets/Script/Player/GameInput.cs
@@ -5,12 +5,19 @@ public class GameInput : MonoBehaviour
 {
     private IA_Player playerInputActions;
     public event EventHandler OnInteractAction;
+    public event EventHandler OnUseRockAction;
     private void Awake()
     {
         playerInputActions = new IA_Player();
         playerInputActions.Player.Enable();
 
         playerInputActions.Player.Interact.performed += Interact_performed;
+        playerInputActions.Player.UseRock.performed += UseRock_performed;
+    }
+
+    private void UseRock_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        OnUseRockAction?.Invoke(this,EventArgs.Empty);
     }
 
     private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
