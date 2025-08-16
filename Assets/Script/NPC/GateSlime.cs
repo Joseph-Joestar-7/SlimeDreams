fba38d110cfdc944e90054e55986d56cb59c6d92
Ritaban Chaudhuri <inrcr27@gmail.com>
2025-08-16 18:58:55 +0530

    Complete NPC dialogue setup

--- Assets/Script/NPC/GateSlime.cs
+++ Assets/Script/NPC/GateSlime.cs
@@ -14,10 +14,87 @@ public class GateSlime : MonoBehaviour, I_Interactable
     private int dialogueIndex;
     private bool isTyping, isDialogueActive;
 
+    public bool canInteract()
+    {
+        return !isDialogueActive;
+    }
     public void Interact()
     {
-        Debug.Log("Hi");
+        if (dialogueData == null || !isDialogueActive)
+        {
+            return;
+        }
+
+        if(isDialogueActive)
+        {
+            NextLine();
+        }
+        else
+        {
+            StartDialogue();
+        }
     }
+
+    void StartDialogue()
+    {
+        isDialogueActive = true;
+        dialogueIndex = 0;
+
+        nameText.SetText(dialogueData.npcName);
+        portraitImage.sprite = dialogueData.npcPotrait;
+
+        dialoguePanel.SetActive(true);
+
+        StartCoroutine(TypeLine());
+        
+    }
+
+    void NextLine()
+    {
+        if (isTyping)
+        {
+            StopAllCoroutines();
+            dialogueText.SetText(dialogueData.dialogueLines[dialogueIndex]);
+            isTyping = false;
+        }
+        else if (++dialogueIndex < dialogueData.dialogueLines.Length)
+        {
+            StartCoroutine(TypeLine()); 
+        }
+        else
+        {
+            EndDialogue();
+        }
+    }
+
+    IEnumerator TypeLine()
+    {
+        isTyping = true;
+        dialogueText.SetText("");
+
+        foreach(char letter in dialogueData.dialogueLines[dialogueIndex])
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(dialogueData.typingSpeed);
+        }
+
+        isTyping=false;
+
+        if(dialogueData.autoProgressLines.Length > dialogueIndex && dialogueData.autoProgressLines[dialogueIndex] )
+        {
+            yield return new WaitForSeconds(dialogueData.autoPorgressDelay);
+            NextLine();
+        }
+    }
+
+    public void EndDialogue()
+    {
+        StopAllCoroutines();
+        isDialogueActive = false;
+        dialogueText.SetText("");
+        dialoguePanel.SetActive(false);
+    }
+
 
 }
     
