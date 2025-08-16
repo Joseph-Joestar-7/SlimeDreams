99ef3ab60f0ebe01cbbe9a02a2bd91cae5f91ee0
Ritaban Chaudhuri <inrcr27@gmail.com>
2025-08-16 18:38:03 +0530

    add gateslime variables

--- Assets/Script/NPC/GateSlime.cs
+++ Assets/Script/NPC/GateSlime.cs
@@ -1,20 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GateSlime : MonoBehaviour,I_Interactable
+public class GateSlime : MonoBehaviour, I_Interactable
 {
+    [SerializeField] private NPCDialogue dialogueData;
+    [SerializeField] private GameObject dialoguePanel;
+    [SerializeField] private TMP_Text dialogueText, nameText;
+    [SerializeField] private Image portraitImage;
+
+    private int dialogueIndex;
+    private bool isTyping, isDialogueActive;
+
     public void Interact()
     {
         Debug.Log("Hi");
     }
-    void Start()
-    {
-        
-    }
 
-    void Update()
-    {
-        
-    }
 }
+    
