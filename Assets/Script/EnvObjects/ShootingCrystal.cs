5b31d89c0ef44327b340a87df1258c14b1852073
Ritaban Chaudhuri <inrcr27@gmail.com>
2025-08-16 11:13:09 +0530

    Update ShootingCrystal.cs

--- Assets/Script/EnvObjects/ShootingCrystal.cs
+++ Assets/Script/EnvObjects/ShootingCrystal.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 public class ShootingCrystal : MonoBehaviour
 {
     private GameObject player;
+    [SerializeField] private GameInput gameInput;
+    [SerializeField] private GameObject projectile;
 
     private void Start()
     {
         player = null;
+
+        gameInput.OnUseRockAction += GameInput_OnUseRockAction;
     }
+
+    private void GameInput_OnUseRockAction(object sender, System.EventArgs e)
+    {
+        if (player == null)
+            return;
+
+        Instantiate()
+    }
+
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D collision)
     {
